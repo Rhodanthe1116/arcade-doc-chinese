ff5de0318f03d47adbaf651ea9487af1ecfada01
Rhodanthe1116 <z0916945857@gmail.com>
2021-04-12 15:11:45 +0000

    add blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
+  title: 'Arcade 中文手冊',
   tagline: 'The tagline of my site',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
@@ -11,9 +11,9 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Arcade 中文手冊',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Arcade 中文手冊 Logo',
         src: 'img/logo.svg',
       },
       items: [
